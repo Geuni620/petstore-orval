70e23a4faedebdabaab9b795d3e079e374b581e3
Geuni <dlrmsgnl620@naver.com>
2024-06-15 01:08:56 +0900

    fix: solve UMD error

--- utils/test/index.tsx
+++ utils/test/index.tsx
@@ -15,5 +15,6 @@ export const customRender = (ui: ReactElement, client?: QueryClient) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export * from '@testing-library/react';
 export { customRender as render };
