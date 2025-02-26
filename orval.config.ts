409619e746dee7ad8357606c441cb3c1b3495166
Geuni <dlrmsgnl620@naver.com>
2025-02-27 01:28:01 +0900

    feat: tag 기준 filter

---
+++ orval.config.ts
@@ -0,0 +1,16 @@
+export default {
+  petstore: {
+    output: {
+      mode: 'tags-split', // split
+      target: './src/api/petstore.ts',
+      schemas: './src/api/models',
+      client: 'fetch',
+    },
+    input: {
+      target: './petstore.yaml',
+      filters: {
+        tags: [/store/],
+      },
+    },
+  },
+};
